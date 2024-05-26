74a0688a3719a1bca467b766487e6d70b6453c93
storytellerF <34095089+storytellerF@users.noreply.github.com>
2024-05-26 16:29:30 +0800

    移除未使用的导入

--- plugin/src/main/java/com/storyteller_f/song/Song.kt
+++ plugin/src/main/java/com/storyteller_f/song/Song.kt
@@ -4,7 +4,6 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
-import org.gradle.api.tasks.Optional
 import java.io.File
 
 interface SongExtension {
